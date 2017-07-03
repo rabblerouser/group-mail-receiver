94f152eebb81b2831e630d174658a78194841d17
phazel <cwebster@thoughtworks.com>
2017-07-03 11:53:51 +1000

    Pull the image when doing a docker build

--- lambda/bin/docker-build-and-deploy.sh
+++ lambda/bin/docker-build-and-deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 
-docker build -t rabblerouser/group-mail-receiver .
+docker build --pull -t rabblerouser/group-mail-receiver .
 docker push rabblerouser/group-mail-receiver
