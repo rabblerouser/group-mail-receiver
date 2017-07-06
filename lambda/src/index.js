7d467f12f5ad4464ee96fe6e303013adcd242a1b
aaron-edwards <edwards.aaronmark@gmail.com>
2017-07-06 19:45:20 +1000

    Fixed the tests that were marked as pending

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -8,24 +8,24 @@ module.exports = (s3, request, uri, secret) => (event, context, callback) => {
         };
     });
 
-    const requestAttempts = emailEvents.map(emailRecord => {
-        s3.getObject(emailRecord, (err, data) => {
-            if(err) {
-                return Promise.reject(err);
-            }
-
-            console.log("EMAIL CONTENTS:", data);
-            const params = {
-                method: 'POST',
-                uri: uri,
-                body: data.Body,
-                json: true,
-                headers: { Authorization: secret }
-            };
-
-            return request(params);
-        });
-    });
+    const requestAttempts = emailEvents.map(emailRecord => (
+        new Promise((resolve, reject) => {
+            s3.getObject(emailRecord, (err, data) => {
+                if(err) {
+                  reject(err);
+                } else {
+                    console.log("EMAIL CONTENTS:", data);
+                    resolve({
+                        method: 'POST',
+                        uri,
+                        body: data.Body,
+                        json: true,
+                        headers: { Authorization: secret }
+                    });
+                }
+            }) 
+        }).then(data => request(data))
+    ))
 
     return Promise.all(requestAttempts)
         .then(() => {
