cc83bf2b0cebeb01f3d67934336895ae8ad4e966
phazel <cwebster@thoughtworks.com>
2017-07-22 21:56:34 +1000

    Remove regex for '+' characters and URI decoding for the S3 object reference.
    This was left over from the example code in the AWS lambda docs. I don't know if we need this or not, but I figure we can put it back if it becomes important.

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -4,7 +4,7 @@ module.exports = (event, context, callback) => {
   const emailReferences = event.Records.map(record => {
     return {
       bucket: record.s3.bucket.name,
-      key: decodeURIComponent(record.s3.object.key.replace(/\+/g, ' ')),
+      key: record.s3.object.key,
     };
   });
   callback(null, emailReferences);
