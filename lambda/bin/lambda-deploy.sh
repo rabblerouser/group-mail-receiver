b737dfc8c60982c6c491bc0b18d684cf105ba963
Cam Jackson <camjackson89@gmail.com>
2017-10-22 22:01:51 +0800

    Make the zip command quieter, to make it easier to debug

--- lambda/bin/lambda-deploy.sh
+++ lambda/bin/lambda-deploy.sh
@@ -8,7 +8,7 @@ echo 'REMOVING DEV DEPENDENCIES'
 npm prune --production
 
 echo 'PACKAGING THE CODE'
-zip -r $PACKAGE_NAME.zip index.js src node_modules/ -x __tests__
+zip --quiet -r $PACKAGE_NAME.zip index.js src node_modules/ -x __tests__
 
 echo 'UPLOADING TO S3'
 aws s3 cp $PACKAGE_NAME.zip s3://rabblerouser-ses-artefacts/lambdas/$PACKAGE_NAME.zip
