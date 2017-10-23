0b4076c22977208c2dd1e5af7d8b51c8ab20639e
Cam Jackson <camjackson89@gmail.com>
2017-10-23 20:07:11 +0800

    Hard-code to use the same artefacts bucket as the other lambdas

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,14 +12,6 @@ variable "domain" {
 variable "ses_verification_token" {
   description = "The token use to verify email domains by SES"
 }
-variable "rabble_rouser_artefact_bucket" {
-  description = "The name of the bucket that Rabble Rouser"
-  default = "rabblerouser-ses-artefacts"
-}
-variable "rabble_rouser_artefact_lambda_directory" {
-  description = "The directory in the Rabble Rouser artefact bucket to put lambdas"
-  default = "lambdas"
-}
 variable "tf_state_backend_bucket" {
   description = "The name of the bucket where we store the terraform state file for this repository"
   default = "tf-state--group-mail-receiver"
