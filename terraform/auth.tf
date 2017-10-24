ea4ad3fbc3864997f7efb94da98496880e92ef79
Cam Jackson <camjackson89@gmail.com>
2017-10-24 22:47:25 +0800

    Break a cyclic dependency between the lambda and its auth token

--- terraform/auth.tf
+++ terraform/auth.tf
@@ -1,7 +1,7 @@
 resource "random_id" "group_mail_receiver_auth_token" {
   keepers = {
     # Generate a new token whenever the lambda version changes
-    lambda_version = "${aws_lambda_function.group_mail_receiver.version}"
+    lambda_version = "${data.aws_s3_bucket_object.group_mailer_zip.version_id}"
   }
 
   # With this length, it's as random as a type-4 UUID
